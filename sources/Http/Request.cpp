5a04abf85fc1a0c110bdb8ab001b02db516d9df1
Rilemko <rilemko@gmail.com>
2024-06-28 22:51:03 +0000

    fix(Request): Fix chunked request

--- sources/Http/Request.cpp
+++ sources/Http/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: mconreau <mconreau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 20:09:26 by mconreau          #+#    #+#             */
-/*   Updated: 2024/06/27 22:28:07 by mconreau         ###   ########.fr       */
+/*   Updated: 2024/06/28 22:46:29 by mconreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,23 +37,60 @@ Request::recv()
 	{
 		if (!String::match("?*\r\n*\r\n", packet))
 			return (this->_status = 400, (void) NULL);
+		return (this->unchunk(packet));
+	}
+	else
+	{
+		if (!String::match("?* ?* HTTP/*\r\n?*\r\n\r\n*", packet))
+			return (this->_status = 400, (void) NULL);
+		
+		size_t			p = packet.find("\r\n\r\n");
+		const string	h = packet.substr(0, p + 2);
+		stringstream	t(h.substr(0, (p = h.find("\r\n") + 2) - 2));
+		string			v;
 
-		unsigned int i;   
-		stringstream ss;
-		ss << hex << packet.substr(0, packet.find("\r\n"));
-		ss >> i;
+		t >> this->_method >> this->_target >> v;
 
-		if (i == 0)
-			return (this->_status = 200, (void) NULL);
-		else
+		if (v != "HTTP/1.1")
+			return (this->_status = 505, (void) NULL);
+		if (this->_target.size() > 2000)
+			return (this->_status = 414, (void) NULL);
+
+		size_t			d = 0;
+
+		if ((d = this->_target.find('?')) != string::npos)
 		{
-			this->_packet += packet.substr(packet.find("\r\n") + 2, i);
-			//cout << ":" << packet << ":" << endl;
-			this->_length += i;
-			return (this->_status = 1, (void) NULL);
+			this->_params = this->_target.substr(d + 1);
+			this->_target = this->_target.substr(0, d);
+		}
+
+		for (size_t e = 0; (e = h.find("\r\n", p)) != string::npos;)
+		{
+			string	lne(h.substr(p, e - p));
+			string 	key(lne.substr(0, d = lne.find(':')));
+			string	val(lne.substr(d + 1));
+
+			if (val.size() > 8000)
+				return (this->_status = 431, (void) NULL);
+			this->_header[String::lowercase(String::strim(key, " "))] = String::strim(val, " \r\n");
+			p = e + 2;
+		}
+
+		if (this->_header.find("transfer-encoding") != this->_header.end() && this->_header["transfer-encoding"] == "chunked")
+		{
+			this->unchunk(packet.substr(packet.find("\r\n\r\n") + 4));
+			this->_status = 2;
+		}
+		else if (this->_method == "POST" || this->_method == "PUT" || this->_method == "PATCH")
+		{
+			stringstream	ss;
+
+			ss << this->getHeader("content-length", String::tostr(18446744073709551615UL));
+			ss >> this->_length;
+			this->_packet = packet.substr(packet.find("\r\n\r\n") + 4, this->_length);
 		}
 	}
-	
+	/*
 	size_t 			p = packet.find("\r\n\r\n");
 	const string 	h(packet.substr(0, p));
 	stringstream	stream(h.substr(0, (p = h.find("\r\n", 0) + 2) - 2));
@@ -92,15 +129,52 @@ Request::recv()
 		string	val(lne.substr(f + 1));
 		this->_header[String::lowercase(String::strim(key, " "))] = String::strim(val, " \r\n");
 
-		stringstream ss;
-		ss << this->getHeader("content-length", "0");
-		ss >> this->_length;
-
-		this->_packet = packet.substr(packet.find("\r\n\r\n") + 4, this->_length);
-
 		if (this->_header.find("transfer-encoding") != this->_header.end() && this->_header["transfer-encoding"] == "chunked")
+		{
+			this->unchunk(packet.substr(packet.find("\r\n\r\n") + 4));
 			this->_status = 2;
+		}
+		else if (this->_method == "POST" || this->_method == "PUT" || this->_method == "PATCH")
+		{
+			stringstream ss;
+			ss << this->getHeader("content-length", "0");
+			ss >> this->_length;
+
+			this->_packet = packet.substr(packet.find("\r\n\r\n") + 4, this->_length);
+		}
 	}
+	*/
+}
+
+void
+Request::unchunk(const string &packet)
+{
+	unsigned int	l = 0;
+
+	for (size_t	e = 0, i = 0, p = 0; (e = packet.find("\r\n", p)) != string::npos;)
+	{
+		if (!(i % 2))
+		{
+			const string	sl = packet.substr(p, e - p);
+
+			if (e == p || sl.find_first_not_of("0123456789abcdef") != string::npos)
+				return (this->_status = 400, (void) NULL);
+
+			stringstream 	ss;
+			ss << hex << sl;
+			ss >> l;
+			if (l == 0)
+				return (this->_status = 200, (void) NULL);
+		}
+		else
+		{
+			this->_packet += packet.substr(p, l);
+			this->_length += l;
+		}
+		p = e + 2;
+		++i;
+	}
+	this->_status = 1;
 }
 
 string
